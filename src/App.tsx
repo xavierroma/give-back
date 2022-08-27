6fdbeb3840ad0921bf1113bab8bbd4e2ae8d27b9
xavierroma <xavier@accointing.com>
2022-08-27 15:58:24 +0200

    Setup mui with basic theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Fab, ThemeProvider} from '@mui/material';
 import './App.css';
+import {theme} from "./materialUiTheme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Fab variant="extended" size="medium" color="primary" aria-label="add">
+          Give back
+        </Fab>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/materialUiTheme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff7100',
+    },
+    secondary: {
+      main: '#008eff',
+    },
+  },
+  shape: {
+    borderRadius: 24,
+  },
+});
+
