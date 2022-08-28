820f7930a77122771464fe3c2a62ca8c7bd56c29
xavierroma <xavier@accointing.com>
2022-08-29 00:20:36 +0200

    Add Buffer to window

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Buffer } from 'buffer';
 import App from './App';
 import '@fontsource/nunito/300.css';
 import '@fontsource/nunito/400.css';
 import '@fontsource/nunito/500.css';
 import '@fontsource/nunito/700.css';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+window.Buffer = Buffer;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
